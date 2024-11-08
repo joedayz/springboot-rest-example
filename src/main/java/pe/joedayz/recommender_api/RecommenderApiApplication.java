9d6926473369f22a2b9f5adc408d707a0eac29e6
Jose Diaz <jose@exoreaction.com>
2024-11-07 20:42:43 -0500

    rest example

---
+++ src/main/java/pe/joedayz/recommender_api/RecommenderApiApplication.java
@@ -0,0 +1,13 @@
+package pe.joedayz.recommender_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommenderApiApplication.class, args);
+	}
+
+}
